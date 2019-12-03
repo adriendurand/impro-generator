25983db0d6b5ea75fcae6dca531a9423d23f044f
Adrien DURAND <adurand.contact@gmail.com>
2019-12-03 17:21:28 +0100

    Add random generation

--- src/components/Match.vue
+++ src/components/Match.vue
@@ -1,20 +1,59 @@
 <template>
-  <div class="hello">
+  <div>
     <h1>{{ msg }}</h1>
     <router-link to="/">
       <span>{{button1}}</span>
     </router-link>
-    THEMES
+    </br>
+    </br>
+    
+    <b>Random</b></br>
+    <!-- <b>Type:</b> {{types[randomNumber(0, types.length)]}} </br>
+    <b>Theme:</b> {{themes[randomNumber(0, types.length)]}} </br>
+    <b>Number of players:</b> {{players[randomNumber(0, types.length)]}} </br> 
+    <b>Duration:</b> {{durations[randomNumber(0, types.length)]}} </br>
+    <b>Categorie:</b> {{categories[randomNumber(0, types.length)]}} </br> </br> -->
+
+    <button v-on:click="createMatch">Random</button> </br></br>
+    <b>Type:</b> {{type}} </br>
+    <b>Theme:</b> {{theme}} </br>
+    <b>Number of players:</b> {{player}} </br> 
+    <b>Duration:</b> {{duration}} </br>
+    <b>Category:</b> {{category}} </br> </br>
+
+    </br>
+    <b>DURATIONS</b>
+    <ul>
+      <li v-for="duration in durations" :key="duration">
+        {{duration}}
+      </li>
+    </ul>
+
+    <b>PLAYERS</b>
+    <ul>
+      <li v-for="player in players" :key="player">
+        {{player}}
+      </li>
+    </ul>
+
+    <b>TYPES</b>
+    <ul>
+      <li v-for="type in types" :key="type">
+        {{type}}
+      </li>
+    </ul>
+
+    <b>THEMES</b>
     <ul>
       <li v-for="theme in themes" :key="theme">
         {{theme}}
       </li>
     </ul>
 
-    CATEGORIES
+    <b>CATEGORIES</b>
     <ul>
-      <li v-for="category in categories" :key="category">
-        {{category}}
+      <li v-for="categoryy in categories" :key="categoryy">
+        {{categoryy}}
       </li>
     </ul>
   </div>
@@ -22,7 +61,11 @@
 
 <script>
 import categories from '../assets/json/categories.json'
+import durations from '../assets/json/durations.json'
+import players from '../assets/json/players.json'
 import themes from '../assets/json/themes.json'
+import types from '../assets/json/types.json'
+
 export default {
   name: 'Match',
   data () {
@@ -30,21 +73,35 @@ export default {
       msg: 'Bienvenue sur votre générateur de match d\'improvisation',
       button1: 'Accueil',
       categories: categories,
-      themes: themes
+      durations: durations,
+      players: players,
+      themes: themes,
+      types: types,
+      category: "",
+      duration: "",
+      player: "",
+      theme: "",
+      type: "",
     }
   },
   created() {
-    //this.fetchData();
+    this.createMatch();
   },
   watch: {
-    //$route: "fetchData"
+    $route: "createMatch"
   },
   methods: {
-    fetchData() {
-      this.categories = [
-        {name: 'Categ1'},
-        {name: 'Categ2'}
-      ];
+    randomNumber : function(min, max){
+      min = Math.ceil(min);
+      max = Math.floor(max);
+      return Math.floor(Math.random() * (max - min)) + min;
+    },
+    createMatch() {
+      this.category = categories[ Math.floor(Math.random() * (categories.length))];
+      this.duration = durations[ Math.floor(Math.random() * (durations.length))];
+      this.player = players[ Math.floor(Math.random() * (players.length))];
+      this.theme = themes[ Math.floor(Math.random() * (themes.length))];
+      this.type = types[ Math.floor(Math.random() * (types.length))];
     }
   }
 }
