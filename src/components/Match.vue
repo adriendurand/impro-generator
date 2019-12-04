3b1e71352e305ba170f0aca6248d92e822d9182e
Adrien DURAND <adurand.contact@gmail.com>
2019-12-04 15:22:55 +0100

    fix

--- src/components/Match.vue
+++ src/components/Match.vue
@@ -1,48 +1,52 @@
 <template>
-  <md-card id="match" class="md-layout-item md-size-50 md-small-size-100">
-    <md-card-header>
-      <div class="md-title">Match</div>
-    </md-card-header>
+  <div>
+    <form novalidate class="md-layout">
+      <md-card id="match" class="md-layout-item md-size-50 md-small-size-100">
+        <md-card-header>
+          <div class="md-title">Match</div>
+        </md-card-header>
 
-    <md-card-content>
-      <div class="md-layout md-gutter">
-        <div class="md-layout-item md-small-size-100">
-          <md-field>
-            <label for="category">Dans la catégorie:</label>
-            <md-textarea id="category" v-model="category"></md-textarea>
-          </md-field>
-        </div>
-        <div class="md-layout-item md-small-size-100">
-          <md-field>
-            <label for="type">Improvisation:</label>
-            <md-input id="type" type="text" v-model="type"/>
-          </md-field>
-        </div>
-        <div class="md-layout-item md-small-size-100">
-          <md-field>
-            <label for="theme">Ayant pour thème:</label>
-            <md-input id="theme" type="text" v-model="theme"/>
-          </md-field>
-        </div>
-        <div class="md-layout-item md-small-size-100">
-          <md-field>
-            <label for="player">Nombre de joueurs:</label>
-            <md-input id="player" type="text" v-model="player"/>
-          </md-field>
-        </div>
-        <div class="md-layout-item md-small-size-100">
-          <md-field>
-            <label for="duration">Pour une durée de:</label>
-            <md-input id="duration" type="text" v-model="duration"/>
-          </md-field>
-        </div>
-      </div> 
-    </md-card-content>
+        <md-card-content>
+          <div class="md-layout md-gutter">
+            <div class="md-layout-item md-small-size-100">
+              <md-field>
+                <label for="category">Dans la catégorie:</label>
+                <md-textarea id="category" v-model="category"></md-textarea>
+              </md-field>
+            </div>
+            <div class="md-layout-item md-small-size-100">
+              <md-field>
+                <label for="type">Improvisation:</label>
+                <md-input id="type" type="text" v-model="type"/>
+              </md-field>
+            </div>
+            <div class="md-layout-item md-small-size-100">
+              <md-field>
+                <label for="theme">Ayant pour thème:</label>
+                <md-input id="theme" type="text" v-model="theme"/>
+              </md-field>
+            </div>
+            <div class="md-layout-item md-small-size-100">
+              <md-field>
+                <label for="player">Nombre de joueurs:</label>
+                <md-input id="player" type="text" v-model="player"/>
+              </md-field>
+            </div>
+            <div class="md-layout-item md-small-size-100">
+              <md-field>
+                <label for="duration">Pour une durée de:</label>
+                <md-input id="duration" type="text" v-model="duration"/>
+              </md-field>
+            </div>
+          </div>
+        </md-card-content>
 
-    <md-card-actions>
-          <md-button class="md-primary" v-on:click="createMatch">Nouvelle manche</md-button>      
-    </md-card-actions>
-  </md-card>
+        <md-card-actions>
+          <md-button class="md-primary" v-on:click="createMatch">Nouvelle manche</md-button>
+        </md-card-actions>
+      </md-card>
+    </form>
+  </div>
 </template>
 
 <script>
--- src/main.js
+++ src/main.js
@@ -14,8 +14,8 @@ Vue.use(VueMaterial)
 new Vue({
   el: '#app',
   router,
-  components: { 
-    App 
+  components: {
+    App
   },
   template: '<App/>'
 })
