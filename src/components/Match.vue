61b85f0a65416b0b81f1c6000e60c026ecd1ef68
Adrien DURAND <adurand.contact@gmail.com>
2019-12-06 12:28:14 +0100

    Fix timer disabled button & unit item refresh

--- src/components/Match.vue
+++ src/components/Match.vue
@@ -9,24 +9,28 @@
         <md-card-content>
           <div class="md-layout md-gutter">
             <div class="md-layout-item md-small-size-100">
-              <span>Dans la catégorie: {{category.title}}</span>          
+              <span>Dans la catégorie: {{category.title}}</span>
               <md-button class="md-primary" :disabled="(category.detail == '')" @click="showDialog = (category.detail != '')"><md-icon>help</md-icon></md-button>
+              <md-button class="md-primary" @click="refreshCategory"><md-icon>refresh</md-icon></md-button>
             </div>
             <div class="md-layout-item md-small-size-100">
               <span>Improvisation:</span>
               <span>{{type}}</span>
+              <md-button class="md-primary" @click="refreshType"><md-icon>refresh</md-icon></md-button>
             </div>
             <div class="md-layout-item md-small-size-100">
               <span>Ayant pour thème:</span>
               <span>{{theme}}</span>
+              <md-button class="md-primary" @click="refreshTheme"><md-icon>refresh</md-icon></md-button>
             </div>
             <div class="md-layout-item md-small-size-100">
               <span>Nombre de joueurs:</span>
               <span>{{player}}</span>
+              <md-button class="md-primary" @click="refreshPlayer"><md-icon>refresh</md-icon></md-button>
             </div>
             <div class="md-layout-item md-small-size-100">
               <span>Pour une durée de:</span>
-              <span>{{duration}} minutes</span>
+              <span>{{duration}} minutes</span><md-button class="md-primary" @click="refreshDuration"><md-icon>refresh</md-icon></md-button>
             </div>
           </div>
         </md-card-content>
@@ -54,7 +58,7 @@
           <md-button class="md-primary" v-on:click="addTimer"><md-icon>add</md-icon></md-button>
         </md-card-actions>
         <md-card-actions>
-          <md-button class="md-primary" :disabled="isPlaying" @click="duration = 1; countdownMin = 1; countdownSec = 0"><md-icon>refresh</md-icon></md-button>
+          <md-button class="md-primary" :disabled="isPlaying" @click="initCountdown"><md-icon>refresh</md-icon></md-button>
         </md-card-actions>
       </div>
 
@@ -107,23 +111,37 @@ export default {
       max = Math.floor(max)
       return Math.floor(Math.random() * (max - min)) + min
     },
-    createMatch () {
+    refreshCategory () {
       this.category = categories[Math.floor(Math.random() * (categories.length))]
+    },
+    refreshDuration () {
       this.duration = durations[Math.floor(Math.random() * (durations.length))]
-      this.player = players[Math.floor(Math.random() * (players.length))]
-      this.theme = themes[Math.floor(Math.random() * (themes.length))]
-      this.type = types[Math.floor(Math.random() * (types.length))]
-
       this.initCountdown()
     },
+    refreshPlayer () {
+      this.player = players[Math.floor(Math.random() * (players.length))]
+    },
+    refreshTheme () {
+      this.theme = themes[Math.floor(Math.random() * (themes.length))]
+    },
+    refreshType () {
+      this.type = types[Math.floor(Math.random() * (types.length))]
+    },
+    createMatch () {
+      this.refreshCategory()
+      this.refreshDuration()
+      this.refreshPlayer()
+      this.refreshTheme()
+      this.refreshType()
+    },
     initCountdown () {
+      this.isMinTimer = false
+      this.duration = 2
       this.countdownMin = this.duration
-      //this.isPlaying = false
       this.countdownSec = 0
       clearInterval(this.x)
     },
     launchCountdown () {
-      //this.isPlaying = true
       var self = this
       var timer = moment().add((self.duration * 60) + 1, 's')
 
@@ -143,7 +161,8 @@ export default {
       if (this.duration > 1) {
         this.duration--
         this.countdownMin = this.duration
-      } else {
+      } 
+      if (this.duration <= 1) {
         this.isMinTimer = true
       }
     },
