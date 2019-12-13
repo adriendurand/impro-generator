a3b32ddae9cd1389ce97c377864cf41526df5ee4
Adrien DURAND <adurand.contact@gmail.com>
2019-12-13 16:09:22 +0100

    Add about page

---
+++ src/components/pages/About.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="about">
+    <md-card id="match" class="md-layout-item md-size-50 md-small-size-100">
+      <md-card-content>
+        <div class="md-layout md-gutter">
+          Made with love by @adurand
+        </div>
+      </md-card-content>
+    </md-card>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'About'
+}
+</script>
+
+<style scoped>
+</style>
